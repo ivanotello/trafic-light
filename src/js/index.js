c9c8348cd84044a925e8a29843a83ab357045894
ivanotello <iodaruosgg@gmail.com>
2020-08-06 04:52:00 +0000

    Trafic light completado

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,47 @@
 export default TraficLight;
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "bootstrap";
 import "../styles/index.scss";
 
-import TraficLight from "./component/TraficLight.js";
+const TraficLight = props => {
+	const [light, setLight] = useState(null);
+
+	const changeLight = selectedLight => {
+		setLight(selectedLight);
+	};
+	let redSelected = "";
+	if (light == "red") {
+		redSelected = "selected";
+	}
+	let yellowSelected = "";
+	if (light == "yellow") {
+		yellowSelected = "selected";
+	}
+	let greenSelected = "";
+	if (light == "green") {
+		greenSelected = "selected";
+	}
+
+	return (
+		<div>
+			<div id="pole" />
+			<div id="lightBox">
+				<div
+					className={"red light " + redSelected}
+					onClick={() => changeLight("red")}
+				/>
+				<div
+					className={"yellow light " + yellowSelected}
+					onClick={() => changeLight("yellow")}
+				/>
+				<div
+					className={"green light " + greenSelected}
+					onClick={() => changeLight("green")}
+				/>
+			</div>
+		</div>
+	);
+};
 
 ReactDOM.render(<TraficLight />, document.querySelector("#app"));
